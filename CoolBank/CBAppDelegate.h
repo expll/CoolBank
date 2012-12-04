62485cd3b51fe36f54c7822904b6b9f7d4cf0ab3
WangLei <wang.lei732@gmail.com>
2012-12-04 15:54:14 +0800

    init

---
+++ CoolBank/CBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAppDelegate.h
+//  CoolBank
+//
+//  Created by  on 12-12-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
